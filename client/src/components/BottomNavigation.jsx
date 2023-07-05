a9766cd749075892eace70f148e350802c239e3d
Thafsi PV <pvthafsi@gmail.com>
2023-07-05 17:08:16 +0530

    Settings and department

--- client/src/components/BottomNavigation.jsx
+++ client/src/components/BottomNavigation.jsx
@@ -1,35 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSolidContact } from "react-icons/bi";
 import { IoPersonAdd } from "react-icons/io5";
+import { FaRegBuilding } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function BottomNavigation() {
+  const [activeTab,setActiveTab]=useState(1)
+  console.log("🚀 ~ file: BottomNavigation.jsx:9 ~ BottomNavigation ~ activeTab:", activeTab)
   return (
     <div className="btm-nav">
-      <button>
+      <button className= {`${activeTab==1?"active":''}`} onClick={()=>setActiveTab(1)}>
         <Link to='/'>
         <BiSolidContact className="h-5 w-5" />
         </Link>
       </button>
-      <button className="active">
+      <button className={`${activeTab==2?"active":''}`} onClick={()=>setActiveTab(2)}>
         <Link to='/addNew'>
           <IoPersonAdd className="h-5 w-5" />
         </Link>
       </button>
-      <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-          />
-        </svg>
+      <button className={`${activeTab==3?"active":''}`} onClick={()=>setActiveTab(3)}>
+       <Link to='/dept'> <FaRegBuilding  className="h-5 w-5"/></Link>
       </button>
     </div>
   );
