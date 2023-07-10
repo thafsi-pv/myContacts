f2283ceee831f3667a80c96d89efc8db4b33bd16
Thafsi PV <pvthafsi@gmail.com>
2023-07-10 16:08:19 +0530

    logo design

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
     <div className="navbar bg-base-100 fixed top-0 z-10">
       <div className="flex-1">
         <Link to='/'>
-          <a className="btn btn-ghost normal-case text-xl">myContacts</a>
+          <p className="flex text-xl px-4 items-baseline font-semibold">my<p className="text-green-500 p-0 m-0 font-bold text-2xl">C</p>ontacts</p>
         </Link>
       </div>
       <div className="flex-none">
