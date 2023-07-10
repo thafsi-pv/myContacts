66317d6b9c41befc780b6ec780e49f0d5f73daf0
Thafsi PV <pvthafsi@gmail.com>
2023-07-10 15:55:15 +0530

    Logo link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BiMenu } from "react-icons/bi";
 import Drawer from "./Drawer";
+import { Link } from "react-router-dom";
 
 function Header() {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
@@ -16,13 +17,19 @@ function Header() {
   return (
     <div className="navbar bg-base-100 fixed top-0 z-10">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">myContacts</a>
+        <Link to='/'>
+          <a className="btn btn-ghost normal-case text-xl">myContacts</a>
+        </Link>
       </div>
       <div className="flex-none">
         <button className="btn btn-square btn-ghost" onClick={openDrawer}>
           <BiMenu className="inline-block w-6 h-6 stroke-current" />
         </button>
-        <Drawer isOpen={isDrawerOpen} onClose={closeDrawer} setDrawerOpen={setDrawerOpen} />
+        <Drawer
+          isOpen={isDrawerOpen}
+          onClose={closeDrawer}
+          setDrawerOpen={setDrawerOpen}
+        />
       </div>
     </div>
   );
