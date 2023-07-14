cd837c27566f267480fd63c40607aac6a4feb483
Thafsi PV <pvthafsi@gmail.com>
2023-07-14 16:08:44 +0530

    shimmer animate

--- client/src/components/ShimmerContacts.jsx
+++ client/src/components/ShimmerContacts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ShimmerContacts = () => {
   return (
-    <div className="flex flex-col p-8  justify-center mt-16 max-h-[700px] lg:w-2/4 m-auto bg-gray-900">
+    <div className="flex flex-col p-8 justify-center mt-16 max-h-[700px] lg:w-2/4 m-auto bg-gray-900">
       <input
         className="borde bg-gray-700 border-gray-400 rounded-lg p-2 mb-4"
         type="text"
@@ -10,7 +10,7 @@ const ShimmerContacts = () => {
       />
       <div className="space-y-4">
         {[...Array(10)].map((_, index) => (
-          <div className="flex items-center" key={index}>
+          <div className="flex items-center animate-pulse" key={index}>
             <div className="h-12 w-12 bg-gray-400 rounded-full"></div>
             <div className="ml-4">
               <div className="h-4 bg-gray-400 w-32 rounded"></div>
