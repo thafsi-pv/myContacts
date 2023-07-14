da093ead92606e1c76ad794484cde891b246588a
Thafsi PV <pvthafsi@gmail.com>
2023-07-14 21:57:00 +0530

    const

--- client/src/const/const.js
+++ client/src/const/const.js
@@ -1,6 +1,6 @@
-// export const CONTACTS_API = "https://my-contacts-snowy.vercel.app/api/contacts";
-// export const DEPARTMENT_API = "https://my-contacts-snowy.vercel.app/api/departments";
-// export const DESIGNATION_API = "https://my-contacts-snowy.vercel.app/api/designation";
-export const CONTACTS_API = "http://localhost:3458/api/contacts";
-export const DEPARTMENT_API = "http://localhost:3458/api/departments";
-export const DESIGNATION_API = "http://localhost:3458/api/designation";
+export const CONTACTS_API = "https://my-contacts-snowy.vercel.app/api/contacts";
+export const DEPARTMENT_API = "https://my-contacts-snowy.vercel.app/api/departments";
+export const DESIGNATION_API = "https://my-contacts-snowy.vercel.app/api/designation";
+// export const CONTACTS_API = "http://localhost:3458/api/contacts";
+// export const DEPARTMENT_API = "http://localhost:3458/api/departments";
+// export const DESIGNATION_API = "http://localhost:3458/api/designation";
