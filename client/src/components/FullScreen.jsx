1e6508a291b0cb8e391773c42bdaa019a229482b
Thafsi PV <pvthafsi@gmail.com>
2023-07-18 13:36:07 +0530

    fullscreen

--- client/src/components/FullScreen.jsx
+++ client/src/components/FullScreen.jsx
@@ -1,29 +1,27 @@
-import React, { useEffect } from "react";
-import "../style/fullScreenComponent.css";
+import React, { useEffect } from 'react';
 
 const FullscreenComponent = ({ children }) => {
-  // useEffect(() => {
-  //     const openFullscreen = () => {
-  //       const element = document.documentElement; // Change to your desired element
-  //       if (element.requestFullscreen) {
-  //         element.requestFullscreen();
-  //       } else if (element.mozRequestFullScreen) {
-  //         element.mozRequestFullScreen();
-  //       } else if (element.webkitRequestFullscreen) {
-  //         element.webkitRequestFullscreen();
-  //       } else if (element.msRequestFullscreen) {
-  //         element.msRequestFullscreen();
-  //       }
-  //     };
+   useEffect(() => {
+    const openFullscreen = () => {
+      const element = document.documentElement; // Change to your desired element
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    };
 
-  //     const isMobile = window.matchMedia('(max-width: 768px)').matches;
-  //     if (isMobile) {
-  //       openFullscreen();
-  //     }
-  //   }, []);
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
+    if (isMobile) {
+      openFullscreen();
+    }
+  }, []);
 
-  //return <>{children}</>;
-  return <div className="full-screen-container">{children}</div>;
+  return <>{children}</>;
 };
 
 export default FullscreenComponent;
