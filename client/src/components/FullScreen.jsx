cd6f651f7cade322617e95e900d24a1a3c558c55
Thafsi PV <pvthafsi@gmail.com>
2023-07-18 12:38:09 +0530

    full screen

--- client/src/components/FullScreen.jsx
+++ client/src/components/FullScreen.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import "../style/fullScreenComponent.css";
 
 const FullscreenComponent = ({ children }) => {
-  useEffect(() => {
-    const openFullscreen = () => {
-      const element = document.documentElement;
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-      } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-      } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-      }
-    };
+  // useEffect(() => {
+  //     const openFullscreen = () => {
+  //       const element = document.documentElement; // Change to your desired element
+  //       if (element.requestFullscreen) {
+  //         element.requestFullscreen();
+  //       } else if (element.mozRequestFullScreen) {
+  //         element.mozRequestFullScreen();
+  //       } else if (element.webkitRequestFullscreen) {
+  //         element.webkitRequestFullscreen();
+  //       } else if (element.msRequestFullscreen) {
+  //         element.msRequestFullscreen();
+  //       }
+  //     };
 
-    openFullscreen();
-  }, []);
+  //     const isMobile = window.matchMedia('(max-width: 768px)').matches;
+  //     if (isMobile) {
+  //       openFullscreen();
+  //     }
+  //   }, []);
 
-  return <>{children}</>;
+  //return <>{children}</>;
+  return <div className="full-screen-container">{children}</div>;
 };
 
 export default FullscreenComponent;
