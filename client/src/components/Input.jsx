b173b155204d39402094835fcd251f34b3cb532f
Thafsi PV <pvthafsi@gmail.com>
2023-07-18 17:48:54 +0530

    input border change

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -12,7 +12,7 @@ function Input({ id, name, type, autoComplete, handleChange, refer, val,placehol
         value={val}
         onChange={(e) => handleChange(e)}
         required
-        className="block w-full rounded-md border-0 px-1 py-2 bg-base-200 text-gray-800 shadow-sm ring-1 ring-gray-600 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+        className="block w-full rounded-md border-0 px-1 py-2 bg-base-200  shadow-sm ring-1 ring-base-500 placeholder:text-gray-400  sm:text-sm sm:leading-6"
       />
     </div>
   );
