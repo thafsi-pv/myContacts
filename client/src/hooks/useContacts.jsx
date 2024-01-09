226a6bf7fef8d0cd2cbf3be1e645135114a1d07b
Thafsi PV <pvthafsi@gmail.com>
2024-01-09 23:03:05 +0530

    useContact custom hook

--- client/src/hooks/useContacts.jsx
+++ client/src/hooks/useContacts.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
 
 function useContacts() {
-  return (
-    <div>useContacts</div>
-  )
+  const getDepartments = async () => {
+    const data = await axios(DEPARTMENT_API);
+    const updatedArray = changeKeyInArray(data.data, keyChanges);
+    setDepartments(updatedArray);
+  };
+
+  const getDesignation = async () => {
+    const data = await axios(DESIGNATION_API);
+    const updatedArray = changeKeyInArray(data.data, keyChanges);
+    setDesignation(updatedArray);
+  };
+
+  const handleDepartmentChange = (selectedOptions) => {
+    setSelectedDept(selectedOptions);
+  };
+  const handleDesignationChange = (selectedOptions) => {
+    setSelectedDesig(selectedOptions);
+  };
+
+  const toggleAccordion = () => {
+    setIsAccordionOpen((prevState) => !prevState);
+  };
+
+  const handleSearchContact = async (e) => {
+    try {
+      const txt = e.target.value;
+      setSearchText(txt);
+      setPage(1);
+      //toggleLoading(true);
+      const designation =
+        selectedDesig.value != undefined ? selectedDesig.value : "";
+      const department =
+        selectedDept.value != undefined ? selectedDept.value : "";
+      const response = await axios.get(`
+    ${CONTACTS_API}/contactGrouped?name=${txt}&designationId=${designation}&departmentId=${department}&page=${page}&pageSize=${pageSize}`);
+      setAllContacts(response?.data?.contactList);
+      // toggleAccordion();
+    } catch (error) {
+      genricError(error);
+    } finally {
+      //toggleLoading(false);
+    }
+  };
+
+  return {
+    getDepartments,
+    getDesignation,
+    handleDepartmentChange,
+    handleDesignationChange,
+    toggleAccordion,
+    handleSearchContact,
+  };
 }
 
-export default useContacts
+export default useContacts;
